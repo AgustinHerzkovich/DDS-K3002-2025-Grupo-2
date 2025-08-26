05f5aaddfb9d892ba95855b0802f04bbf6deb508
EzeSu1 <esu@frba.utn.edu.ar>
2025-08-26 11:00:44 -0300

    UI implementado con html y js de FuenteDinamica

--- fuenteDinamica/src/main/java/domain/FuenteDinamicaAplicacion.java
+++ fuenteDinamica/src/main/java/domain/FuenteDinamicaAplicacion.java
@@ -2,7 +2,10 @@ package domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FuenteDinamicaAplicacion {
@@ -11,4 +14,13 @@ public class FuenteDinamicaAplicacion {
     SpringApplication.run(FuenteDinamicaAplicacion.class, args);
   }
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*"); //establece que front puede hacer peticiones al back, donde puede definir los metodos y headers permitidos
+      }
+    };
+  }
 }
