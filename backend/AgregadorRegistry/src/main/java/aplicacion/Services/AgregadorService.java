ccf90311bf388de08993157909abe586ccbb9522
nicopiacentini <npiacentini@frba.utn.edu.ar>
2025-11-13 19:38:31 -0300

    comienzo agregadorRegistry

---
+++ backend/AgregadorRegistry/src/main/java/aplicacion/Services/AgregadorService.java
@@ -0,0 +1,12 @@
+package aplicacion.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgregadorService {
+
+
+    public Boolean chequearAgregadorExistente(String agregadorID) {
+        return true; //a arreglar luego
+    }
+}
